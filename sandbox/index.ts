9179064f147963538752af7a9380c5b8d896b85e
Shubham Jain <shubham.jain@insider.in>
2023-01-11 01:17:51 +0530

    implement item | pipelining

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,21 +2,23 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1950
-		// engine: { cylinders: 8 },
-		// owner: null || '',
-		// service: undefined || ''
 	});
-
-	const car = await client.hGetAll('car');
-
-	if (Object.keys(car).length === 0) {
-		console.log('Car not found, respond with 404');
-		return;
-	}
-
-	console.log(car);
+	await client.hSet('car2', {
+		color: 'white',
+		year: 1960
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1965
+	});
+	const result = await Promise.all([
+		client.hGetAll('car1'),
+		client.hGetAll('car2'),
+		client.hGetAll('car3')
+	]);
+	console.log(result);
 };
 run();
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,9 +1,9 @@
-import type { CreateItemAttrs } from '$services/types';
-import { client } from '$services/redis';
-import { serialize } from './serialize';
-import { genId } from '$services/utils';
 import { itemsKey } from '$services/keys';
+import { client } from '$services/redis';
+import type { CreateItemAttrs } from '$services/types';
+import { genId } from '$services/utils';
 import { deserialize } from './deserialize';
+import { serialize } from './serialize';
 
 export const getItem = async (id: string) => {
 	const item = await client.hGetAll(itemsKey(id));
@@ -15,7 +15,18 @@ export const getItem = async (id: string) => {
 	return deserialize(id, item);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => client.hGetAll(itemsKey(id)));
+	const results = await Promise.all(commands);
+
+	return results.map((result, i) => {
+		if (Object.keys(result).length === 0) {
+			return null;
+		}
+
+		return deserialize(ids[i], result);
+	});
+};
 
 export const createItem = async (attrs: CreateItemAttrs) => {
 	const id = genId();
