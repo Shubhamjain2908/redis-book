35a3a65feade66fc282344c74e2b7af54a6fd500
Shubham Jain <shubham.jain@insider.in>
2023-01-11 03:58:48 +0530

    fix auth flow

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,9 +1,25 @@
-import { usernamesUniqueKey, usersKey } from '$services/keys';
+import { usernamesKey, usernamesUniqueKey, usersKey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+	// use the username arg to look over the Persons userId with the username sorted set
+	const decimalId = await client.zScore(usernamesKey(), username);
+
+	// make sure we actually got an Id from set
+	if (!decimalId) {
+		throw new Error("User doesn't exists");
+	}
+
+	// convert it back to hex
+	const id = decimalId.toString(16);
+	// lookup user hash from that id
+	const user = await client.hGetAll(usersKey(id));
+
+	// deserialize & return hash
+	return deserialize(id, user);
+};
 
 export const getUserById = async (id: string) => {
 	const user = await client.hGetAll(usersKey(id));
@@ -21,6 +37,10 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 
 	await client.hSet(usersKey(id), serialize(attrs));
 	await client.sAdd(usernamesUniqueKey(), attrs.username);
+	await client.zAdd(usernamesKey(), {
+		value: attrs.username,
+		score: parseInt(id, 16)
+	});
 
 	return id;
 };
