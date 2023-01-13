63ac16e90a280aaec7629d9725cf5fb17ba9ea6d
Shubham Jain <shubham.jain@insider.in>
2023-01-14 04:43:19 +0530

    using buildClientProxy for handling expired locks

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,11 +1,11 @@
 import { randomBytes } from 'crypto';
 import { client } from './client';
 
-export const withLock = async (key: string, cb: (signal: any) => any) => {
+export const withLock = async (key: string, cb: (redisClient: Client, signal: any) => any) => {
 	// Intialize a few variable to control retry behaviour
 	const retryDelayMs = 100;
 	let retries = 20;
-
+	const timeoutMs = 2000;
 	// Generate a random value to store at the lock key
 	const token = randomBytes(6).toString('hex');
 	// Create a lock key
@@ -17,7 +17,7 @@ export const withLock = async (key: string, cb: (signal: any) => any) => {
 		// Try to do a SET NX operation
 		const acquired = await client.set(lockKey, token, {
 			NX: true,
-			PX: 2000
+			PX: timeoutMs
 		});
 
 		if (!acquired) {
@@ -33,8 +33,10 @@ export const withLock = async (key: string, cb: (signal: any) => any) => {
 			};
 			setTimeout(() => {
 				signal.expired = true;
-			}, 2000);
-			const result = await cb(signal);
+			}, timeoutMs);
+
+			const proxiedClient = buildClientProxy(timeoutMs);
+			const result = await cb(proxiedClient, signal);
 			return result;
 		} finally {
 			// Unset the locked set
@@ -43,7 +45,23 @@ export const withLock = async (key: string, cb: (signal: any) => any) => {
 	}
 };
 
-const buildClientProxy = () => {};
+type Client = typeof client;
+const buildClientProxy = (timeoutMs: number) => {
+	const startTime = Date.now();
+
+	const handler = {
+		get(target: Client, prop: keyof Client) {
+			if (Date.now() >= startTime + timeoutMs) {
+				throw new Error('Lock has expired.');
+			}
+
+			const value = target[prop];
+			return typeof value === 'function' ? value.bind(target) : value;
+		}
+	};
+
+	return new Proxy(client, handler) as Client;
+};
 
 const pause = (duration: number) => {
 	return new Promise((resolve) => {
