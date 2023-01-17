b7f9d2600eee3fc1d68b732cd360fdc99c1fe090
Shubham Jain <shubham.jain@insider.in>
2023-01-18 03:09:49 +0530

    implement search

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,1 +1,29 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import { itemsIndexKey } from '$services/keys';
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+
+export const searchItems = async (term: string, size: number = 5) => {
+	// doing fuzzy search
+	const cleaned = term
+		.replace(/[^a-zA-Z0-9 ]/g, '')
+		.trim()
+		.split(' ')
+		.map((word) => (word ? `%${word}%` : ''))
+		.join(' ');
+
+	// Look at cleaned and make sure it is valid
+	if (cleaned === '') {
+		return [];
+	}
+
+	// Use the client to do the actual search
+	const results = await client.ft.search(itemsIndexKey(), cleaned, {
+		LIMIT: {
+			from: 0,
+			size
+		}
+	});
+
+	// Deserialize and return the result
+	return results.documents.map(({ id, value }) => deserialize(id, value as any));
+};
